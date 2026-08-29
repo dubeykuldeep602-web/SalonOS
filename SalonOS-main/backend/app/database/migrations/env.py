82de31cb69ad59f8aca0558b48272b8c5b23e79b
Kuldeep dubey <dubeykuldeep602@gmail.com>
2026-08-30 02:31:37 +0530

    Update website

--- SalonOS-main/backend/app/database/migrations/env.py
+++ SalonOS-main/backend/app/database/migrations/env.py
@@ -19,7 +19,7 @@ config = context.config
 if config.config_file_name is not None:
     fileConfig(config.config_file_name)
 
-config.set_main_option("sqlalchemy.url", settings.SQLALCHEMY_DATABASE_URI)
+config.set_main_option("sqlalchemy.url", settings.SQLALCHEMY_DATABASE_URI.replace("%", "%%"))
 
 target_metadata = Base.metadata
 
